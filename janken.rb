6f81cf140c751db86a5473a9b0a4e2065f09af3b
kaori kimura <tkhy_1125@icloud.com>
2022-03-22 13:49:55 +0900

    課題提出

---
+++ janken.rb
@@ -0,0 +1,78 @@
+# プレイヤー(自分)に「0~2」を入力させるロジックを書きます。
+class Player
+  def hand
+    # プレイヤーにじゃんけんの手を選択させる文章を表示させます。
+    puts "数字を入力してください。"
+    puts "0:グー, 1:チョキ, 2:パー"
+    # 変数「input_hand」にプレイヤーの入力値を代入します。
+    # ヒント：getsメソッドについて調べてみましょう。
+    input_hand =gets.chomp
+    # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
+    while true
+      if input_hand == "0" || input_hand == "1" || input_hand == "2"
+        return  input_hand.to_i
+      else
+        puts "0〜2の数字を入力してください。"
+        puts "0:グー, 1:チョキ, 2:パー"
+        input_hand =gets.chomp
+      end
+    end
+  end
+end
+
+
+class Enemy
+  def hand
+  enemy_hand =  rand(3)
+
+  end
+end
+# プレイヤー(自分)が入力した「0~2」と、敵がランダムで生成した「0~2」をじゃんけんをさせて、その結果をコンソール上に出力するロジックを書きます。
+class Janken
+  def pon(player_hand, enemy_hand)
+
+    janken = ["グー", "チョキ", "パー"]
+
+    puts "相手の手は#{janken[enemy_hand]}です。"
+
+    if player_hand == enemy_hand
+      puts "あいこ"
+      return true
+      #「true」を返してじゃんけんを繰り返し実行させます。
+      # ヒント：「return」を使って戻り値を返すことができます。しかし、Rubyでは戻り値を返す場合、「return」を省略するのが一般的です。
+    elsif (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
+      #「あなたの勝ちです」を出力します。
+      puts "あなたの勝ちです"
+      return false
+      #「false」を返してじゃんけんを終了させます。
+    else
+      puts "あなたの負けです"
+      return false
+      #「あなたの負けです」を出力します。
+      #「false」を返してじゃんけんを終了させます。
+    end
+  end
+end
+# じゃんけんゲームを実行するロジックを書きます。
+class GameStart
+  # selfを使用することで、GameStartをインスタンス化することなく、クラス名を使ってjankenponメソッドを呼び出せます。
+  def self.jankenpon
+    # 変数「player」にPlayerをインスタンス化したものを代入します。
+    #Playerクラスの
+    player = Player.new
+    # 変数「enemy」にEnemyをインスタンス化したものを代入します。
+    enemy = Enemy.new
+    # 変数「janken」にJankenをインスタンス化したものを代入します。
+    janken = Janken.new
+    # 変数「next_game」に「true」を代入しましょう。
+    # 「next_game」が「false」だと繰り返し処理を終了し、「true」だと繰り返し処理を継続します。
+    next_game = true
+    while next_game
+      # 変数「next_game」にじゃんけんを実行して返ってきた値(戻り値)を代入します。
+      #「janken.pon(player.hand, enemy.hand)」でじゃんけんを実行しています。
+      next_game = janken.pon(player.hand, enemy.hand)
+    end
+  end
+end
+# クラス名を使ってjankenponメソッドを呼び出します。
+GameStart.jankenpon
